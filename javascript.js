3c76fb0e1ca015aa794e0d7e1d18b632c891144f
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2024-03-05 04:16:27 -0800

    Feat: add changeCellToX function

--- javascript.js
+++ javascript.js
@@ -1,39 +1,59 @@
-const gameboard = {
+const gameBoard = {   //module as an object as per assignment
   boardState: [],
   init: function() {
     this.addBoardToArray();
-    this.bindEvents();
-    this.render();
-    this.test();
+/*     this.bindEvents();
+ *//*     this.render();
+ */    this.test();
   },
   addBoardToArray: function() {
-    for (let i = 0; i<9; i++) {
+    for (let i = 0; i < 9; i++) {
       this.boardState[i] = document.getElementById(`cell${i}`).firstElementChild.textContent;
     } 
   },
-  bindEvents: function() {
+/*   bindEvents: function() {
     console.log("bindEvents")
   },
   render: function() {
     console.log("render")
   },
-  test: function() {
+ */  test: function() {
     console.log(this.boardState);
   }
 }
-gameboard.init();
-/* console.log(gameboard.cacheDom());
-
-const test2 = document.getElementById('cell1');
-test2.textContent = "f";
- */
+gameBoard.init();
 
 
 
-const player1 = {
 
+const playerOne = {
+  init: function() {
+    this.changeCellToX();
+  },
+  changeCellToX: function() {
+    for (let i = 0; i < 9; i++) {
+      const cell = document.getElementById(`cell${i}`);
+      cell.addEventListener('click', test3);
+
+      function test3() {
+        cell.firstElementChild.textContent = "x";
+      }
+    }
+  },
+  variable: document.getElementById('cell1'),  //test
+}
+playerOne.init();  //test
+console.log(playerOne.variable);  //test
+
+gameBoard.addBoardToArray();  //works after using playerOne function
+gameBoard.test();  //works
+
+
+
+
+const playerTwo = {
 }
 
-const player2 = {
-  
-}
+
+//how to store variables in object modules
+//why even store gameboard in array if I can just change textcontent
