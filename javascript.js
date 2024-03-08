b0bd0bc392b1f71780c9bf7e67be928b756b6f1b
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2024-03-08 05:57:53 -0800

    Chore: add IIFEs around object literals

--- javascript.js
+++ javascript.js
@@ -1,113 +1,134 @@
+const board = (function() {   //IIFE
 
-const gameBoard = {   //module as an object i.e. object literal pattern - as per assignment
-  boardState: [],
-  init: function() {
-    this.addBoardToArray();
-    this.consoleLog();
-  },
-  addBoardToArray: function() {   //adds board state to an Array,
-    for (let i = 0; i < 9; i++) {   //don't need this function but will keep for future reference
-      this.boardState[i] = document.getElementById(`cell${i}`).firstElementChild.textContent;
-    } 
-  },
-  consoleLog: function() {
-    console.log(this.boardState);
+  const gameBoard = {   //object literal pattern - as per assignment
+    boardState: [],
+    init: function() {
+      this.addBoardToArray();
+      this.consoleLog();
+    },
+    addBoardToArray: function() {   //adds board state to an Array,
+      for (let i = 0; i < 9; i++) {   //don't need this function but will keep for future reference
+        this.boardState[i] = document.getElementById(`cell${i}`).firstElementChild.textContent;
+      } 
+    },
+    consoleLog: function() {
+      console.log(this.boardState);
+    }
   }
-}
-gameBoard.init();
+  gameBoard.init();
+
+  return {gameBoard}   //returning the whole object so nothing private
+})();
 
 //////////////////////////////
 
-const playerOne = {
-  init: function() {
-    this.changeCellToX();
-  },
-  nextPlayer: "",
-  changeCellToX: function() {
-    for (let i = 0; i < 9; i++) {
-      const cell = document.getElementById(`cell${i}`);
-      cell.addEventListener('click', addX);
-      cell.addEventListener('click', this.updateArray);
+const one = (function() {
 
-      function addX() {
-        if (cell.firstElementChild.textContent === "" && playerOne.nextPlayer === "One") {
-          cell.firstElementChild.textContent = "x";
-        } else {}
+  const playerOne = {   //object literal pattern
+    init: function() {
+      this.changeCellToX();
+    },
+    nextPlayer: "",
+    changeCellToX: function() {
+      for (let i = 0; i < 9; i++) {
+        const cell = document.getElementById(`cell${i}`);
+        cell.addEventListener('click', addX);
+        cell.addEventListener('click', this.updateArray);
+
+        function addX() {
+          if (cell.firstElementChild.textContent === "" && playerOne.nextPlayer === "One") {
+            cell.firstElementChild.textContent = "x";
+          } else {}
+        }
       }
+    },
+    updateArray: function() {   //don't need to update the array for game to work
+      if (playerOne.nextPlayer === "One") {
+        board.gameBoard.addBoardToArray();
+        board.gameBoard.consoleLog();
+      } else {}
     }
-  },
-  updateArray: function() {   //don't need to update the array for game to work
-    if (playerOne.nextPlayer === "One") {
-      gameBoard.addBoardToArray();
-      gameBoard.consoleLog();
-    } else {}
   }
-}
-playerOne.init();
+  playerOne.init();
+
+  return {playerOne}   //returning the whole object so nothing private
+})();
 
 //////////////////////////////
 
-const playerTwo = {
-  init: function() {
-    this.changeCellToO();
-  },
-  nextPlayer: "",
-  changeCellToO: function() {
-    for (let i = 0; i < 9; i++) {
+const two = (function() {
 
-      const cell = document.getElementById(`cell${i}`);
-      cell.addEventListener('click', addO);
-      cell.addEventListener('click', this.updateArray);
+  const playerTwo = {   //object literal pattern
+    init: function() {
+      this.changeCellToO();
+    },
+    nextPlayer: "",
+    changeCellToO: function() {
+      for (let i = 0; i < 9; i++) {
 
-      function addO() {
-        if (cell.firstElementChild.textContent === "" && playerTwo.nextPlayer === "Two") {
-          cell.firstElementChild.textContent = "o";
-        } else {}
+        const cell = document.getElementById(`cell${i}`);
+        cell.addEventListener('click', addO);
+        cell.addEventListener('click', this.updateArray);
+
+        function addO() {
+          if (cell.firstElementChild.textContent === "" && playerTwo.nextPlayer === "Two") {
+            cell.firstElementChild.textContent = "o";
+          } else {}
+        }
       }
+    },
+    updateArray: function() {   //don't need to update the array for game to work
+      if (playerTwo.nextPlayer === "Two") {
+        board.gameBoard.addBoardToArray();
+        board.gameBoard.consoleLog();
+      } else {}
     }
-  },
-  updateArray: function() {   //don't need to update the array for game to work
-    if (playerTwo.nextPlayer === "Two") {
-      gameBoard.addBoardToArray();
-      gameBoard.consoleLog();
-    } else {}
   }
-}
-playerTwo.init();
+  playerTwo.init();
+
+return {playerTwo}   //returning the whole object so nothing private
+})();
 
 //////////////////////////////
 
-const playOrder = {
-  init: function() {
-    this.firstPlayer();
-    this.changeCondition();
-  },
-  firstPlayer: function() {
-    if (playerOne.nextPlayer === "" && playerTwo.nextPlayer === "") {
-      playerOne.nextPlayer = "One";
-      playerTwo.nextPlayer = "One";
-      console.log("First move: " + playerOne.nextPlayer);
-    } else {}
-  },
-  changeCondition: function() {
-    for (let i = 0; i < 9; i++) {
-      const cell = document.getElementById(`cell${i}`);
-      cell.addEventListener('click', alternate);
+const order = (function() {
 
-      function alternate() {
-         if (playerOne.nextPlayer === "One" && playerTwo.nextPlayer === "One") {
-          playerOne.nextPlayer = "Two";
-          playerTwo.nextPlayer = "Two";
-          console.log("Next move: " + playerOne.nextPlayer);
-        } else if (playerOne.nextPlayer === "Two" && playerTwo.nextPlayer === "Two") {
-          playerOne.nextPlayer = "One";
-          playerTwo.nextPlayer = "One";
-          console.log("Next move: " + playerOne.nextPlayer);
-        } else {}
+  const playOrder = {   //object literal pattern - private as I have not returned it
+    init: function() {
+      this.firstPlayer();
+      this.changeCondition();
+    },
+    firstPlayer: function() {
+      if (one.playerOne.nextPlayer === "" && two.playerTwo.nextPlayer === "") {
+        one.playerOne.nextPlayer = "One";
+        two.playerTwo.nextPlayer = "One";
+        console.log("First move: " + one.playerOne.nextPlayer);
+      } else {}
+    },
+    changeCondition: function() {
+      for (let i = 0; i < 9; i++) {
+        const cell = document.getElementById(`cell${i}`);
+        cell.addEventListener('click', alternate);
+
+        function alternate() {
+          if (one.playerOne.nextPlayer === "One" && two.playerTwo.nextPlayer === "One") {
+            one.playerOne.nextPlayer = "Two";
+            two.playerTwo.nextPlayer = "Two";
+            console.log("Next move: " + one.playerOne.nextPlayer);
+          } else if (one.playerOne.nextPlayer === "Two" && two.playerTwo.nextPlayer === "Two") {
+            one.playerOne.nextPlayer = "One";
+            two.playerTwo.nextPlayer = "One";
+            console.log("Next move: " + one.playerOne.nextPlayer);
+          } else {}
+        }
       }
     }
   }
-}
-playOrder.init();
+  playOrder.init();
+
+})();
+
+//////////////////////////////
 
 
+//can i return only one function from an object???
